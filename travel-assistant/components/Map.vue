e9f579906cf0aadd00e35c5652ec19e7f13d3f32
Ryan Horrmann <ryanhorrmann123@gmail.com>
2024-06-04 11:50:50 +0200

    hiding tomtom key

--- travel-assistant/components/Map.vue
+++ travel-assistant/components/Map.vue
@@ -4,21 +4,24 @@
 
 <script>
 import tt from "@tomtom-international/web-sdk-maps";
-import '@tomtom-international/web-sdk-maps/dist/maps.css';
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
 const center = { lat: 43.769562, lng: 11.255814 };
+const config = useRuntimeConfig();
 export default {
   mounted() {
     const map = tt.map({
-      key: "JstOGCglq4XYdxHK0KHBC5T68MTMb7ji",
+      key: config.public.tomKey,
       container: "map",
       center: center,
       zoom: 15,
     });
 
-    const marker = new tt.Marker({ color: '#afc125'}).setLngLat(center).addTo(map);
+    const marker = new tt.Marker({ color: "#afc125" })
+      .setLngLat(center)
+      .addTo(map);
 
     map.addControl(new tt.FullscreenControl());
     map.addControl(new tt.NavigationControl());
-},
+  },
 };
 </script>
